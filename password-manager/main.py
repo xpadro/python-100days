0a82395d73e9630eae99b95077ef0702746ffa37
xpadro <xpadro.sw@gmail.com>
2024-06-25 08:30:40 +0200

    finish password manager

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,18 +1,53 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    letters_list = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    symbols_list = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    numbers_list = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    password_list = letters_list + symbols_list + numbers_list
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_input.insert(0, password)
+
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save():
-    with open("data.txt", "a") as f:
-        email = email_input.get()
-        web = website_input.get()
-        password = password_input.get()
-        f.write(f"{web} | {email} | {password}\n")
-        website_input.delete(0, END)
-        password_input.delete(0, END)
+    email = email_input.get()
+    web = website_input.get()
+    password = password_input.get()
+
+    is_empty = len(web) == 0 or len(password) == 0
+    if is_empty:
+        messagebox.showerror(title="Oops", message="Some information is missing")
+    else:
+        is_ok = messagebox.askokcancel(title=web, message=f"These are the details entered: \nEmail: {email}"
+                                                          f"\nPassword: {password}\nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", "a") as f:
+                f.write(f"{web} | {email} | {password}\n")
+                website_input.delete(0, END)
+                password_input.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -46,7 +81,7 @@ email_input.insert(0, "xavi@gmail.com")
 password_input = Entry(width=21)
 password_input.grid(row=3, column=1)
 
-generate_button = Button(text="Generate")
+generate_button = Button(text="Generate", command=generate_password)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
