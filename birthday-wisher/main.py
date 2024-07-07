6750ac66fa36c44741b36a2908a11f1875ccc5e5
xpadro <xpadro.sw@gmail.com>
2024-07-07 11:48:36 +0200

    implement birthday wisher

---
+++ birthday-wisher/main.py
@@ -0,0 +1,29 @@
+import smtplib
+import datetime as dt
+import random
+
+email = "your_email"
+password = "your_app_password"
+
+
+def get_quote():
+    with open("quotes.txt", "r") as f:
+        chosen_quote = f.read().splitlines()
+        return random.choice(chosen_quote)
+
+
+def send_quote(quote):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(
+            from_addr=email,
+            to_addrs=email,
+            msg=f"Subject:Today quote\n\n{quote}")
+
+
+now = dt.datetime.now()
+if now.weekday() == 6:
+    with open("quotes.txt", "r") as f:
+        today_quote = get_quote()
+        send_quote(today_quote)
