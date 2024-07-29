e72a398692a8949fa060c84de814bbbb6c2f5d62
xpadro <xpadro.sw@gmail.com>
2024-07-29 09:01:32 +0200

    simplify data generation

--- quizz-app/data.py
+++ quizz-app/data.py
@@ -12,17 +12,6 @@ question_data = []
 try:
     response = requests.get(URL, params=PARAMETERS)
     response.raise_for_status()
-
-    for result in response.json()["results"]:
-        new_question = {
-            "category": result["category"],
-            "type": result["type"],
-            "difficulty": result["difficulty"],
-            "question": result["question"],
-            "correct_answer": result["correct_answer"],
-            "incorrect_answers": result["incorrect_answers"]
-        }
-        question_data.append(new_question)
-
+    question_data = response.json()["results"]
 except:
     print("Exception while calling opentdb API")
