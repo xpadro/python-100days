adec2f750c897f1d8bcdf3117c715c7f52fa1ffe
xpadro <xpadro.sw@gmail.com>
2024-07-30 08:53:09 +0200

    show questions in ui

--- quizz-app/ui.py
+++ quizz-app/ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -18,7 +20,8 @@ class QuizInterface:
             125,
             text="Some Question Text",
             fill=THEME_COLOR,
-            font=("Arial", 20, "italic"))
+            font=("Arial", 20, "italic"),
+            width=280)
 
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
@@ -30,4 +33,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.next_question()
+
         self.window.mainloop()
+
+    def next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
