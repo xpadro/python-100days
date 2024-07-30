6d330b6381cf92cd02d8738f17a0a6136cd67a92
xpadro <xpadro.sw@gmail.com>
2024-07-30 08:14:17 +0200

    unescape question text

--- quizz-app/main.py
+++ quizz-app/main.py
@@ -1,10 +1,11 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+import html
 
 question_bank = []
 for question in question_data:
-    question_text = question["question"]
+    question_text = html.unescape(question["question"])
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
