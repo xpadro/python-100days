4beb69b8c7d596f2fe8b5cbb3119c16a87917704
xpadro <xpadro.sw@gmail.com>
2024-08-15 13:40:41 +0200

    integrate twilio API

--- stock-news/main.py
+++ stock-news/main.py
@@ -1,11 +1,17 @@
 import os
 import requests
+from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_SID')
+TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
+TWILIO_VIRTUAL_PHONE = os.environ.get('TWILIO_VIRTUAL_PHONE')
+TWILIO_TARGET_PHONE = os.environ.get('TWILIO_TARGET_PHONE')
 
 
 def get_stock():
@@ -40,22 +46,18 @@ def get_top_articles():
     return response.json()['articles'][:3]
 
 
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
+def send_sms(message):
+    """ Use Twilio API to send an SMS message to the specified phone
 
+    :param message:
+    """
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_TOKEN)
 
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    client.messages.create(
+        body=message,
+        from_=TWILIO_VIRTUAL_PHONE,
+        to=TWILIO_TARGET_PHONE
+    )
 
 
 def main():
@@ -66,9 +68,14 @@ def main():
     positive_difference = abs(yesterday_closing_price - day_before_closing_price)
     diff_percentage = (positive_difference / yesterday_closing_price) * 100
 
-    if diff_percentage > 5:
+    if diff_percentage > 1:
         first_3_articles = get_top_articles()
-        print(first_3_articles)
+
+        first_3_article_summaries = [(item['title'], item['description']) for item in first_3_articles]
+
+        for summary in first_3_article_summaries:
+            msg = f"Headline: {summary[0]}.\nBrief: {summary[1]}"
+            send_sms(msg)
 
 
 main()
