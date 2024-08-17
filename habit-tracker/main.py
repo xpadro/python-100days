b1a113719ccb07940128707869f088cebfefd4f3
xpadro <xpadro.sw@gmail.com>
2024-08-17 07:58:37 +0200

    implement add pixel to graph

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,19 +1,31 @@
 import requests
 
-API_CREATE_USER = "https://pixe.la/v1/users"
-API_CREATE_GRAPH = "https://pixe.la/v1/users/xpadro/graphs"
+USERNAME = "xpadro"
 USER_TOKEN = "ad3nw987wn98en"
+
+API_CREATE_USER = "https://pixe.la/v1/users"
+API_CREATE_GRAPH = f"https://pixe.la/v1/users/{USERNAME}/graphs"
+API_ADD_PIXEL = f"https://pixe.la/v1/users/{USERNAME}/graphs/graph1"
 
 
 def create_user():
     parameters = {
         "token": USER_TOKEN,
-        "username": "xpadro",
+        "username": USERNAME,
         "agreeTermsOfService": "yes",
         "notMinor": "yes"
     }
 
     response = requests.post(url=API_CREATE_USER, json=parameters)
+    return response
+
+
+def call_api(url, parameters):
+    headers = {
+        "X-USER-TOKEN": USER_TOKEN
+    }
+
+    response = requests.post(url=url, json=parameters, headers=headers)
     return response
 
 
@@ -26,17 +38,20 @@ def create_graph():
         "color": "sora"
     }
 
-    headers = {
-        "X-USER-TOKEN": USER_TOKEN
+    return call_api(API_CREATE_GRAPH, parameters)
+
+
+def add_pixel(date, num_commits):
+    parameters = {
+        "date": date,
+        "quantity": num_commits
     }
 
-    response = requests.post(url=API_CREATE_GRAPH, json=parameters,  headers=headers)
-    return response
+    return call_api(API_ADD_PIXEL, parameters)
 
 
-# User is already created
-#content = create_user()
-# print(content.text)
+# content = create_user()
+# content = create_graph()
+content = add_pixel("20240816", "2")
 
-content = create_graph()
 print(content.text)
