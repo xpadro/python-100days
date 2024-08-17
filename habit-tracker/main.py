210a18ec0a94a6c20128fdbcc6a0bd50459175cd
xpadro <xpadro.sw@gmail.com>
2024-08-17 08:29:30 +0200

    implement update pixel

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -8,7 +8,7 @@ USER_TOKEN = "ad3nw987wn98en"
 API_BASE_URL = "https://pixe.la/v1"
 API_CREATE_USER = f"{API_BASE_URL}/users"
 API_CREATE_GRAPH = f"{API_BASE_URL}/users/{USERNAME}/graphs"
-API_ADD_PIXEL = f"{API_BASE_URL}/users/{USERNAME}/graphs/graph1"
+API_PIXEL = f"{API_BASE_URL}/users/{USERNAME}/graphs/graph1"
 
 
 def create_user():
@@ -23,7 +23,7 @@ def create_user():
     return response
 
 
-def call_api(url, parameters):
+def call_post_api(url, parameters):
     headers = {
         "X-USER-TOKEN": USER_TOKEN
     }
@@ -41,21 +41,41 @@ def create_graph():
         "color": "sora"
     }
 
-    return call_api(API_CREATE_GRAPH, parameters)
+    return call_post_api(API_CREATE_GRAPH, parameters)
 
 
-def add_pixel(date, num_commits):
+def add_pixel(add_date, num_commits):
     parameters = {
-        "date": date,
+        "date": add_date,
         "quantity": num_commits
     }
 
-    return call_api(API_ADD_PIXEL, parameters)
+    return call_post_api(API_PIXEL, parameters)
+
+
+def update_pixel(update_date, num_commits):
+    parameters = {
+        "quantity": num_commits
+    }
+
+    headers = {
+        "X-USER-TOKEN": USER_TOKEN
+    }
+
+    response = requests.put(url=f"{API_PIXEL}/{update_date}", json=parameters, headers=headers)
+    return response
+
+
+def format_date(date_to_format):
+    return date_to_format.strftime("%Y%m%d")
 
 
 # content = create_user()
 # content = create_graph()
+# content = add_pixel(format_date(dt.now()), "2")
 
-today = dt.now().strftime("%Y%m%d")
-content = add_pixel(today, "2")
+
+date = dt(year=2024, month=8, day=16)
+content = update_pixel(format_date(date), "3")
+
 print(content.text)
