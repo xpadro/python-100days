45e37f7e5d88f3d0b263da3dcb1db7cdecb3db9f
xpadro <xpadro.sw@gmail.com>
2024-08-30 13:45:43 +0200

    prompt date

--- billboard/main.py
+++ billboard/main.py
@@ -4,19 +4,18 @@ from bs4 import BeautifulSoup
 BILLBOARD_BASE_URL = "https://www.billboard.com/charts/hot-100"
 
 
-def get_song_names(html):
+def get_song_names(html: BeautifulSoup) -> list:
     return [item.getText().strip() for item in html.select('li ul li h3')]
 
 
-def get_song_artists(html):
+def get_song_artists(html: BeautifulSoup) -> list:
     return [item.getText().strip() for item in html.select('li ul li span.a-no-trucate')]
 
 
 def get_songs():
     top_songs = []
 
-    # date = input("Insert date (yyyy-mm-dd):")
-    date = "2012-01-01"
+    date = input("Insert date (yyyy-mm-dd):")
     response = requests.get(f"{BILLBOARD_BASE_URL}/{date}")
 
     soup = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
