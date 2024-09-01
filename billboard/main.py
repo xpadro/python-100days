54a5d7941156d994e9b87abfb298878b93bdfee6
xpadro <xpadro.sw@gmail.com>
2024-09-01 21:41:48 +0200

    get spotify url for each song

--- billboard/main.py
+++ billboard/main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from bs4 import BeautifulSoup
 import spotipy
+from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
 BILLBOARD_BASE_URL = "https://www.billboard.com/charts/hot-100"
@@ -14,29 +15,21 @@ def get_song_names(html: BeautifulSoup) -> list:
     return [item.getText().strip() for item in html.select('li ul li h3')]
 
 
-def get_song_artists(html: BeautifulSoup) -> list:
-    return [item.getText().strip() for item in html.select('li ul li span.a-no-trucate')]
-
-
-def get_songs():
+def get_songs(song_date: str) -> list:
     top_songs = []
 
-    date = input("Insert date (yyyy-mm-dd):")
-    response = requests.get(f"{BILLBOARD_BASE_URL}/{date}")
-
+    response = requests.get(f"{BILLBOARD_BASE_URL}/{song_date}")
     soup = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
+    song_names = get_song_names(soup)
 
-    songs = get_song_names(soup)
-    artists = get_song_artists(soup)
-
-    for i in range(len(songs)):
-        top_songs.append(f"{songs[i]} {artists[i]}")
+    for i in range(len(song_names)):
+        top_songs.append(song_names[i])
 
     return top_songs
 
 
-def auth_to_spotify():
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+def auth_to_spotify() -> Spotify:
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         client_id=SPOTIFY_CLIENT_ID,
         client_secret=SPOTIFY_CLIENT_SECRET,
@@ -44,11 +37,25 @@ def auth_to_spotify():
         cache_path="token.txt"
     ))
 
-    result = sp.current_user()
-    print(result)
+
+def get_song_url(spotify, song, year):
+    try:
+        result = spotify.search(q=f"track:{song} year:{year}", type="track", limit=1)
+        return result['tracks']['items'][0]['uri']
+    except IndexError:
+        print(f"Ignoring song. '{song}' does not exist")
 
 
-songs_data = get_songs()
-print(songs_data)
+date = input("Insert date (yyyy-mm-dd):")
+year = date.split("-")[0]
 
-auth_to_spotify()
+songs_data = get_songs(date)
+
+sp = auth_to_spotify()
+songs = []
+
+for song_data in songs_data:
+    song_url = get_song_url(sp, song_data, year)
+    songs.append(song_url)
+
+print(songs)
